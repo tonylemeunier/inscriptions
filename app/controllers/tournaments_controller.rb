9d8a54e8ced414043e54ff8432d43567b9e9959f
Cyrielle Lemeunier <dev@lemeunier.org>
2016-06-28 12:17:56 +0200

    methodes controller_tournament

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,10 +1,11 @@
 class TournamentsController < ApplicationController
+  before_action :set_tournament, only: [:show, :edit, :update, :delete]
+
   def index
     @tournaments = Tournament.all
   end
 
   def show
-    @tournament = Tournament.find(params[:id])
   end
 
   def new
@@ -16,18 +17,23 @@ class TournamentsController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
+    @tournament.update(tournament_params)
+    redirect_to tournament_path(@tournament)
   end
 
   def delete
-
+    @tournament.destroy
+    redirect_to tournaments_path
   end
 
   private
+
+  def set_tournament
+    @tournament = Tournament.find(params[:id])
+  end
 
   def tournament_params
     params.require(:tournament).permit(:city, :date, :tableau, :serie, :price1, :price2, :price3, :club)
