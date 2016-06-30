e5ff715b50085d2c6b1d75a9675ddb7f912b2a21
Cyrielle Lemeunier <dev@lemeunier.org>
2016-06-30 14:55:56 +0200

    eidt le credit d'un joueur

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,47 @@
 class PlayersController < ApplicationController
+
+  before_action :set_player, only: [:show, :edit, :update, :delete]
+
+  def index
+    @players = Player.all
+  end
+
+  def show
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    @player.save
+    redirect_to players_path
+  end
+
+  def edit
+  end
+
+  def update
+    credit = @player.credit
+    credit_sup = params["credit_sup"].to_i
+    new_credit = credit + credit_sup
+    @player.update(:credit => new_credit)
+    redirect_to players_path
+  end
+
+  def delete
+    @player.destroy
+    redirect_to players_path
+  end
+
+  private
+
+  def set_player
+    @player = Player.find(params[:id])
+  end
+
+  def player_params
+    params.require(:player).permit(:first_name, :last_name, :licence, :credit)
+  end
 end
