4ed890779dd2ff8616c87a98f76ca048a66c1d92
Cyrielle Lemeunier <dev@lemeunier.org>
2016-08-03 17:50:25 +0200

    tentative modal liste inscrits

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -25,7 +25,7 @@ class RegistrationsController < ApplicationController
   def create
     @registration = Registration.new(registration_params)
     @registration.save
-    redirect_to index_registrations_by_tournament_tournament_path
+    redirect_to tournaments_path
   end
 
   def edit
