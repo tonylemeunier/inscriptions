35dafd4d2c1fc1b13a7dd3324829fb9857d890f7
Cyrielle Lemeunier <dev@lemeunier.org>
2016-08-04 16:19:55 +0200

    add coms et affichage JS du com si D ou Mx

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -1,2 +1,58 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  console.log('JS test')
+// when a tableau is selected
+  $('#registration_tableau1').change(function() {
+    // cache les 5 élements
+    $('#com1').hide();
+    $('#com2').hide();
+    $('#com3').hide();
+    // Value of tableau selected
+    var tableau = $('#registration_tableau1').val();
+
+    if(tableau == "DH" || tableau == "DD" || tableau == "DMx" ){
+      $('#com1').show();
+    } else {
+      $('#com1').hide();
+      $('#com2').hide();
+      $('#com3').hide();
+    }
+  });
+
+  $('#registration_tableau2').change(function() {
+    // cache les 5 élements
+    $('#com1').hide();
+    $('#com2').hide();
+    $('#com3').hide();
+    // Value of tableau selected
+    var tableau = $('#registration_tableau2').val();
+
+    if(tableau == "DH" || tableau == "DD" || tableau == "DMx" ){
+      $('#com2').show();
+    } else {
+      $('#com1').hide();
+      $('#com2').hide();
+      $('#com3').hide();
+    }
+  });
+
+  $('#registration_tableau3').change(function() {
+    // cache les 5 élements
+    $('#com1').hide();
+    $('#com2').hide();
+    $('#com3').hide();
+    // Value of tableau selected
+    var tableau = $('#registration_tableau3').val();
+
+    if(tableau == "DH" || tableau == "DD" || tableau == "DMx" ){
+      $('#com3').show();
+    } else {
+      $('#com1').hide();
+      $('#com2').hide();
+      $('#com3').hide();
+    }
+  });
+
+})
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804075617) do
+ActiveRecord::Schema.define(version: 20160804134205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20160804075617) do
     t.integer  "price"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "com1"
+    t.string   "com2"
+    t.string   "com3"
   end
 
   add_index "registrations", ["player_id"], name: "index_registrations_on_player_id", using: :btree
