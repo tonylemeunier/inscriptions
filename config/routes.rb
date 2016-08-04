a20fc18753e944bd7f9437ec444427d773f8603e
Cyrielle Lemeunier <dev@lemeunier.org>
2016-08-04 15:07:07 +0200

    test credit du joueur, et màj du joueur après registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
+  get '/credit-insuffisant' => 'pages#credit-insuffisant'
   resources :players
   resources :tournaments do
     resources :registrations
