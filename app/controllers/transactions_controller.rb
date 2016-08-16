16f39d253f9b22ee2ffcb69f67a607dc05b4a348
Cyrielle Lemeunier <dev@lemeunier.org>
2016-08-16 21:24:06 +0200

    index transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,9 @@
 class TransactionsController < ApplicationController
+  def index
+    @player = Player.find(params["player_id"])
+    @transactions_by_player = Transaction.joins(:player).
+                                          where(:player_id => params["player_id"])
+# raise
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/credit-insuffisant' => 'pages#credit-insuffisant'
   get '/comment-ca-fonctionne' => 'pages#comment-ca-fonctionne'
-  resources :players
+  resources :players do
+    resources :transactions
+  end
   resources :tournaments do
     resources :registrations
   end
