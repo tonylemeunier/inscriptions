1d7a23c9c703a907a132b7bf4737bbc1c57afb02
Cyrielle Lemeunier <dev@lemeunier.org>
2016-08-17 11:32:40 +0200

    suppression of datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require bootstrap-datepicker
+
 //= require_tree .
 
 $(document).ready(function(){
-  event.preventDefault();
-  $('input[type="date"]').datepicker({
-    format: "dd/mm/yyyy" });
+
 
   $(window).scroll(function() {
       $('#fixed_div').css('top', $(this).scrollTop());
