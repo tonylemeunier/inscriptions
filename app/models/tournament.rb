956fa268b6e3c67961068bf2e19fc554d10b49c8
Cyrielle Lemeunier <dev@lemeunier.org>
2016-08-23 10:05:43 +0200

    validations tournaments

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,9 @@
 class Tournament < ActiveRecord::Base
   has_many :registrations
+
+  validates :city, presence: true
+  validates :date, presence: true
+  validates :price1, presence: true
+  validates :serie, presence: true
+  validates :tableau, presence: true
 end
